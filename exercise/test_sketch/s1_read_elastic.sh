07b184217d090dcc60f5cb6ec065db1739603f56
vagrant <vagrant@p4>
2020-11-23 12:14:30 +0000

    new file:   exercise/test_sketch/elastic_meas.sh 	modified:   exercise/test_sketch/s1_read_elastic.sh

--- exercise/test_sketch/s1_read_elastic.sh
+++ exercise/test_sketch/s1_read_elastic.sh
@@ -1,28 +1,15 @@
 #!/bin/sh
 
-#echo register_read  old_cms_estimate | simple_switch_CLI --thrift-port 9090
-#echo register_read  old_cm_sketch_r1 | simple_switch_CLI --thrift-port 9090
-#echo register_read  old_cm_sketch_r2 | simple_switch_CLI --thrift-port 9090
-#echo register_read  old_cm_sketch_r3 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_flowID_1 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_p_vote_1 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_flag_1 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_total_vote_1 | simple_switch_CLI --thrift-port 9090
 
-#echo register_read  new_cms_estimate | simple_switch_CLI --thrift-port 9090
-#echo register_read  new_cm_sketch_r1 | simple_switch_CLI --thrift-port 9090
-#echo register_read  new_cm_sketch_r2 | simple_switch_CLI --thrift-port 9090
-#echo register_read  new_cm_sketch_r3 | simple_switch_CLI --thrift-port 9090
 echo register_read  cm_sketch1_r1 | simple_switch_CLI --thrift-port 9090
-#echo register_read  cm_sketch1_r2 | simple_switch_CLI --thrift-port 9090
-#echo register_read  cm_sketch1_r3 | simple_switch_CLI --thrift-port 9090
+
+echo register_read  heavy_counters_flowID_2 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_p_vote_2 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_flag_2 | simple_switch_CLI --thrift-port 9090
+echo register_read  heavy_counters_total_vote_2 | simple_switch_CLI --thrift-port 9090
 
 echo register_read  cm_sketch2_r1 | simple_switch_CLI --thrift-port 9090
-#echo register_read  cm_sketch2_r2 | simple_switch_CLI --thrift-port 9090
-#echo register_read  cm_sketch2_r3 | simple_switch_CLI --thrift-port 9090
-
-
-#echo register_read  old_cm_sketch_r1 | simple_switch_CLI --thrift-port 9091
-#echo register_read  old_cm_sketch_r2 | simple_switch_CLI --thrift-port 9091
-#echo register_read  old_cm_sketch_r3 | simple_switch_CLI --thrift-port 9091
-#echo register_read  new_cm_sketch_r1 | simple_switch_CLI --thrift-port 9091
-#echo register_read  new_cm_sketch_r2 | simple_switch_CLI --thrift-port 9091
-#echo register_read  new_cm_sketch_r3 | simple_switch_CLI --thrift-port 9091
-#echo register_read  last_timestamp | simple_switch_CLI --thrift-port 9091
-#echo register_read  last_timestamp | simple_switch_CLI --thrift-port 9091
